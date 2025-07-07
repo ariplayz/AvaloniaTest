5353d053a0a6f4c58a5230b8713c96fd6423609c
Ari Cummings <ari@aricummings.com>
2025-07-07 18:49:43 -0400

    finished

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Tmds.DBus.Protocol;
@@ -11,8 +12,11 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
-    public void ClickHandler(object sender, RoutedEventArgs args){
-        
+    public void Home(object sender, RoutedEventArgs e)
+    {
+        string HelloWorld;
+        HelloWorld = "Hello World";
+        Console.WriteLine(HelloWorld);
     }
     
 }
